cecf8a772b346a2ffe25e7deea09dbfe050d4f64
hadil belhaj hassin <88197008+12355545@users.noreply.github.com>
2023-10-12 08:45:57 +0100

    Update database.py

--- backend/microservices/admin/database.py
+++ backend/microservices/admin/database.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import declarative_base
  
 import urllib
 
-params = urllib.parse.quote_plus("driver={ODBC Driver 17 for SQL Server}; server=192.168.38.122,1433; database=users;UID=sa;PWD=yourStrong(!)Password")
+params = urllib.parse.quote_plus("driver={ODBC Driver 17 for SQL Server}; server=mysql-db,1433; database=users;UID=sa;PWD=yourStrong(!)Password")
 engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
 
  
